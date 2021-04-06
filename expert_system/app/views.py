d59c09cdfce5e174ccbe7f3f2b9f8bdbbc9be695
ebresafegaga <ebresafegaga@outlook.com>
2021-04-07 00:34:06 +0100

    done with UI

--- expert_system/app/views.py
+++ expert_system/app/views.py
@@ -6,17 +6,39 @@ from .forms import ClassifyForm
 # Create your views here.
 
 def index (request): 
-    return  render (request, 'app/index.html')
+    return render (request, 'app/index.html')
  
 def classify (request): 
-    return render (request, 'app/classify.html')
+    return render (request,  'app/classify.html')
+
+# <<< machine learning classification algorithm here >>> 
+def do_classify (): 
+    return 'benign'
 
 def result (request): 
-    # if request.method == 'POST': 
-    #     form = ClassifyForm (request.POST)
-    #     if form.is_valid (): 
-    #         return HttpResponse("Thanks")
-    # else: 
-    #     form = NameForm()
+    context = {} 
+    id = request.POST.get ('id', 0) 
+    context['id'] = id
 
-    return render (request, 'app/result.html')
+    radius = request.POST.get ('radius', 0.0000) 
+    context['radius'] = radius
+
+    perimeter = request.POST.get ('perimeter', 0.0000) 
+    context['perimeter'] = perimeter
+
+    area = request.POST.get ('area', 0.0000) 
+    context['area'] = area
+
+    smoothness = request.POST.get ('smoothness', 0.0000) 
+    context['smoothness'] = smoothness
+
+    compactness = request.POST.get ('compactness', 0.0000) 
+    context['compactness'] = compactness
+
+    concavity = request.POST.get ('concavity', 0.0000) 
+    context['concavity'] = concavity
+
+    dignostic = do_classify () # <<<< machine learning function here
+    context['diagnostic'] = dignostic 
+
+    return render (request, 'app/result.html', context)
