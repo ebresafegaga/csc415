887b6986fb946c5b500cb1bd6375182e789d3270
ebresafegaga <ebresafegaga@outlook.com>
2021-04-07 09:00:40 +0100

    features str -> float

--- expert_system/app/views.py
+++ expert_system/app/views.py
@@ -10,14 +10,22 @@ def classify (request):
     return render (request,  'app/classify.html')
 
 # <<< machine learning classification algorithm here >>> 
-# This function should take a list of features and 
+# This function should take a dictionary of features and 
 # return a diagnostic of either 'malignant' or 'benign'
-# for now, it just defualts to benign.
-def do_classify (): 
-    return 'benign'
+# for now, it just some fake logic.
+def do_classify (features): 
+    features = {k: float (v) for k, v in features.items () }
+    if (features['area'] + features['radius']) > 100: 
+        return 'benign'
+    else:
+        return 'malignant'
 
 def result (request): 
     context = {} 
+
+    # Honest worry: what would be the type of the values gotten from the POST request
+    # if it's a string, we'd have to convert it to a number? 
+    # what about dynamic typing? 
     id = request.POST.get ('id', 0) 
     context['id'] = id
 
@@ -39,7 +47,7 @@ def result (request):
     concavity = request.POST.get ('concavity', 0.0000) 
     context['concavity'] = concavity
 
-    dignostic = do_classify () # <<<< machine learning function here
+    dignostic = do_classify (context) # <<<< machine learning function here
     context['diagnostic'] = dignostic 
 
     return render (request, 'app/result.html', context)
